84f8fca933a2eb3ccf36755fa6a3aef2bf408c8a
Jydoo <adeboyejojide5@gmail.com>
2025-09-11 22:05:52 +0100

    rendernow

--- js/onboarding1.js
+++ js/onboarding1.js
@@ -1,4 +1,4 @@
-const API_URL = "https://calmspace-api.onrender.com/api/auth" || "http://localhost:5000/api/auth";
+const API_URL = "https://calmspace-api.onrender.com/api/auth" ;
 
 // Handle token from Google OAuth redirect
 document.addEventListener('DOMContentLoaded', function() {
